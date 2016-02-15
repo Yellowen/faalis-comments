019db4569b1fb55cc3dc2e1a156672d764b658c4
Sameer Rahmani <lxsameer@gnu.org>
2016-02-15 15:53:16 +0330

    comment model migration improved

--- db/migrate/20160215122104_create_faalis_comments_faalis_comments_comments.rb
+++ db/migrate/20160215122104_create_faalis_comments_faalis_comments_comments.rb
@@ -2,12 +2,12 @@ class CreateFaalisCommentsFaalisCommentsComments < ActiveRecord::Migration
   def change
     create_table :faalis_comments_faalis_comments_comments do |t|
       t.integer :user_id
-      t.string, :name
+      t.string :name
       t.string :email
       t.text :content
-      t.string :parent_id
-      t.string :integer
-      t.integer :likes
+      t.integer :parent_id
+      t.integer :likes, default: 0
+      t.boolean :deleted, default: false
 
       t.timestamps null: false
     end
